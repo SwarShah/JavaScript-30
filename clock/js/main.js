887d6ed30fbc92c39b43db16e8143064205bde91
Swar Shah <swar12894@gmail.com>
2016-12-11 01:57:11 -0500

    Clock: JS optimization

--- clock/js/main.js
+++ clock/js/main.js
@@ -6,16 +6,24 @@ function setDate(){
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const secondsDegrees = calculateDegress(seconds);
+  setDegress(secondHand, secondsDegrees);
 
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + 90;
-  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const minsDegrees = calculateDegress(mins);
+  setDegress(minHand, minsDegrees);
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  const hoursDegrees = calculateDegress(hours, 12);
+  setDegress(hourHand, hoursDegrees);
+}
+
+function calculateDegress(time, segments = 60){
+  return ((time / segments) * 360) + 90;
+}
+
+function setDegress(hand, degrees){
+  hand.style.transform = `rotate(${degrees}deg)`;
 }
 
 setInterval(setDate, 1000);
